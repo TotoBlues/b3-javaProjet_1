63d817fd02f7a5cbc0403d2435bd93946b2d1158
fiskax <33343818+fiskax@users.noreply.github.com>
2017-11-15 10:20:37 +0100

    v1
    parsing

--- src/ParsingCsv.java
+++ src/ParsingCsv.java
@@ -1,4 +1,19 @@
-
 public class ParsingCsv {
 
+	public static String[] returnCsv(String lineCsv) {
+		String[] result = lineCsv.split(";");
+		return result;
+	}
+
+	public static void main(String args[]) {
+		String[] resultLine = returnCsv(new String(
+				"VT66556555;The Witcher 3;Incarnez un tueur de monstres et vivez son épopée !;jeu video;32,12"));
+
+		String codeCsv = resultLine[0];
+		String nomCsv = resultLine[1];
+		String descriptionCsv = resultLine[2];
+		String categorieCsv = resultLine[3];
+		String prixCsv = resultLine[4] + "€";
+
+	}
 }
