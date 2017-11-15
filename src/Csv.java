129d556ae11ee03a93fa57dd99fcb7d26f980c9a
cedric clemenceau <cedric.clemenceau@epitech.eu>
2017-11-15 12:57:36 +0100

    src/Csv.java

--- src/Csv.java
+++ src/Csv.java
@@ -1,10 +1,24 @@
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Csv {
-	private int fd;
-	
-	public Csv(String file) {
-		File csvFile;
+	public static void readCsv(String file) {
+		Path fichierCsv = Paths.get(file);
+		try (Stream<String> linesFile = Files.lines(fichierCsv)) {
+			linesFile.forEach(System.out::println);
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+		}
 		
+	}
+	
+	public static void main(String [] args) {
+			System.out.println(args[0]);
+		Csv.readCsv(args[0]);
 	}
 }
