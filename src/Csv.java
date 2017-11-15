fdfec886bebbb97459d8ee89f577628292c43883
cedric clemenceau <cedric.clemenceau@epitech.eu>
2017-11-15 14:45:02 +0100

    parse ligne csv ok

--- src/Csv.java
+++ src/Csv.java
@@ -5,18 +5,21 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class Csv {
+	private String csvPath;
+	private String categorieFilter;
+	private String etiquetteName;
+	private String fichesName;
 	public static void readCsv(String file) {
-		Path fichierCsv = Paths.get(file);
+		Path fichierCsv = Paths.get(file); // file descriptor
 		try (Stream<String> linesFile = Files.lines(fichierCsv)) {
-			linesFile.forEach(System.out::println);
+			linesFile.forEach(item -> ParsingCsv.returnCsv(item));
 		}
 		catch (IOException e)
 		{
 			System.out.println(e);
-		}
-		
+		}	
 	}
-	
+
 	public static void main(String [] args) {
 			System.out.println(args[0]);
 		Csv.readCsv(args[0]);
--- src/ParsingCsv.java
+++ src/ParsingCsv.java
@@ -1,8 +1,10 @@
 public class ParsingCsv {
 
 	public static String[] returnCsv(String lineCsv) {
-		System.out.println(lineCsv);
 		String[] result = lineCsv.split(";");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(result[i]);
+		}
 		return result;
 	}
 }
